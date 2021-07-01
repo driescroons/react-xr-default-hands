7de4ae87653796931ca92f911059bff20c03f84c
driescroons <dries.croons@panenco.com>
2021-07-01 22:15:20 +0200

    feat: 2 boxes

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import './styles.css'
 
 import { OrbitControls } from '@react-three/drei'
 import { DefaultXRControllers, VRCanvas, XRController } from '@react-three/xr'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { VRButton } from 'three/examples/jsm/webxr/VRButton'
 
 import { DefaultHandControllers } from './DefaultHandControllers'
-import { BoxBufferGeometry, MeshBasicMaterial, Object3D, XRHandedness } from 'three'
+import { BoxBufferGeometry, MeshBasicMaterial, Object3D, Vector3, XRHandedness } from 'three'
 import { Grab } from './Grab'
 import { HandModel } from './HandModel'
 import { Suspense } from 'react'
@@ -21,6 +21,7 @@ import Pear from './Pear'
 function HandControllersExample() {
   const [interacting, setInteracting] = useState<{ [key in XRHandedness]?: Object3D | null }>({ left: null, right: null })
   const [models, setModels] = useState<HandModel[]>([])
+
   return (
     <VRCanvas
       onCreated={(args) => {
@@ -30,7 +31,6 @@ function HandControllersExample() {
       <OrbitControls />
       <ambientLight intensity={0.3} />
       <pointLight position={[0, 5, 5]} intensity={1} />
-      <pointLight position={[0, 5, -5]} intensity={1} />
       <DefaultHandControllers
         onConnect={(models) => {
           setModels(models)
@@ -49,7 +49,7 @@ function HandControllersExample() {
         models={models}
         interacting={interacting}>
         <Suspense fallback={null}>
-          <Crate position={[0, 1.5, 0]} scale={[0.1, 0.1, 0.1]} />
+          <Crate position={[Math.cos(0.5 + Math.PI / 2) * 0.5, 1.5, Math.sin(0.5 + Math.PI / 2) * 0.5]} scale={[0.1, 0.1, 0.1]} />
         </Suspense>
       </Grab>
       <Grab
@@ -61,43 +61,7 @@ function HandControllersExample() {
         models={models}
         interacting={interacting}>
         <Suspense fallback={null}>
-          <Eggplant position={[Math.cos(0) * 0.5, 1.5, Math.sin(0) * 0.5]} scale={[0.5, 0.5, 0.5]} />
-        </Suspense>
-      </Grab>
-      <Grab
-        onChange={({ isGrabbed, controller, object }) => {
-          if (isGrabbed) {
-            setInteracting({ ...interacting, [controller.inputSource.handedness]: object })
-          }
-        }}
-        models={models}
-        interacting={interacting}>
-        <Suspense fallback={null}>
-          <Apple position={[Math.cos(Math.PI / 2) * 0.5, 1.5, Math.sin(Math.PI / 2) * 0.5]} scale={[0.5, 0.5, 0.5]} />
-        </Suspense>
-      </Grab>
-      <Grab
-        onChange={({ isGrabbed, controller, object }) => {
-          if (isGrabbed) {
-            setInteracting({ ...interacting, [controller.inputSource.handedness]: object })
-          }
-        }}
-        models={models}
-        interacting={interacting}>
-        <Suspense fallback={null}>
-          <Pineapple position={[Math.cos(Math.PI) * 0.5, 1.5, Math.sin(Math.PI) * 0.5]} scale={[0.5, 0.5, 0.5]} />
-        </Suspense>
-      </Grab>
-      <Grab
-        onChange={({ isGrabbed, controller, object }) => {
-          if (isGrabbed) {
-            setInteracting({ ...interacting, [controller.inputSource.handedness]: object })
-          }
-        }}
-        models={models}
-        interacting={interacting}>
-        <Suspense fallback={null}>
-          <Pear position={[Math.cos(Math.PI + Math.PI / 2) * 0.5, 1.5, Math.sin(Math.PI + Math.PI / 2) * 0.5]} scale={[0.5, 0.5, 0.5]} />
+          <Crate position={[Math.cos(-0.5 + Math.PI / 2) * 0.5, 1.5, Math.sin(-0.5 + Math.PI / 2) * 0.5]} scale={[0.1, 0.1, 0.1]} />
         </Suspense>
       </Grab>
     </VRCanvas>
